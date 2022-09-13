e8736a39c802c1d31c6506e222bc629930674fa4
Cesare Ferrari <cesare@maryandferrari.com>
2022-09-13 17:41:19 -0400

    Add fields

--- app/services/import_customers.rb
+++ app/services/import_customers.rb
@@ -1,6 +1,15 @@
 class ImportCustomers
   def call(file)
-    options = {key_mapping: {first_name: :first, last_name: :last, buyer_user_id: :username}, remove_unmapped_keys: true}
+    options = {key_mapping: {first_name: :first,
+                             last_name: :last,
+                             buyer_user_id: :username,
+                             street_1: :address1,
+                             street_2: :address2,
+                             delivery_city: :city,
+                             delivery_state: :state,
+                             delivery_zipcode: :zip,
+                             delivery_country: :country},
+               remove_unmapped_keys: true}
 
     # Test data in db/data/customers-partial.csv
     SmarterCSV.process(file, options) do |row|
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -11,4 +11,15 @@ class CustomerTest < ActiveSupport::TestCase
     customer = Customer.new(first: "Jenny", last: "Smith")
     assert "Jenny Smith", customer.name
   end
+
+  test "customer has an address" do
+    customer = Customer.new(address1: "855 George Edward Via", address2: "Unit 316",
+      city: "Christiansburg", state: "VA", zip: "24073", country: "United States")
+    assert_equal "855 George Edward Via", customer.address1
+    assert_equal "Unit 316", customer.address2
+    assert_equal "Christiansburg", customer.city
+    assert_equal "VA", customer.state
+    assert_equal "24073", customer.zip
+    assert_equal "United States", customer.country
+  end
 end

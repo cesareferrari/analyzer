a4ffa01721764104541e2a3507bc58620eebe4c0
Cesare Ferrari <cesare@maryandferrari.com>
2022-10-11 15:57:16 -0400

    Average order

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -11,4 +11,16 @@ class OrderTest < ActiveSupport::TestCase
   test "sum of all orders" do
     assert_equal 11.23, Order.total_net
   end
+
+  test "average of 2 orders" do
+    # Average of net_cents column
+    # 1123 cents, 2 orders, average 5.615, rounded to 5.61
+    assert_equal 5.61, Order.average_net
+  end
+
+  test "average of 3 orders" do
+    customers(:one).orders.create!(net_cents: 300)
+    # net cents: 1423 / 3
+    assert_equal 4.74, Order.average_net
+  end
 end
